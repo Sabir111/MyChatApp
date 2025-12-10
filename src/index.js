bad2f95df9b78789eb7791b177431005a022f8e4
Sabir <sabiralitanveer6@gmail.com>
2025-12-10 16:44:01 +0530

    add passwrod bcrypt jsonwebtoken

--- src/index.js
+++ src/index.js
@@ -1,8 +1,22 @@
 import dotenv from "dotenv";
 import connectDB from "./db/index.js";
+import app from "./app.js";
 
 dotenv.config({
     path: "./env"
 });
+PORT = process.env.PORT || 8000;
+connectDB().
+then(() => {
+    app.on("error", (error) =>{
+        console.error("Server error:", error);
+        throw error;
+    })
+    app.listen(PORT, () => {
+        console.log(`Server is running on port ${PORT}`);
+    })
 
-connectDB();
+}).
+catch((error) => {
+    console.error("Failed to connect to the database:", error);
+})
