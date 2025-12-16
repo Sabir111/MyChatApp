36214f15b321c90fd2a64a4e481f22ce714c87cf
Sabir <sabiralitanveer6@gmail.com>
2025-12-16 18:45:39 +0530

    add login - logout controller

--- src/controllers/user.controllers.js
+++ src/controllers/user.controllers.js
@@ -1,9 +1,24 @@
 import {asyncHandler} from '../utils/asyncHandler.js';
 import { User } from '../models/user.models.js';
+import { ApiError } from '../utils/ApiError.js';
+import { ApiResponse } from '../utils/ApiResponse.js';
+
+const generateAccessAndRefreshToken = async(userId)=>{
+    try {
+        const user = await User.findById(userId);
+        const accessToken = user.generateAccessToken()
+        const refreshToken = user.generateRefreshToken()
+
+        user.refreshToken = refreshToken;
+        await user.save({validateBeforeSave:false});
+        return {accessToken, refreshToken};
+    } catch (error) {
+        throw new ApiError(500, "Token generation failed");
+    }
+}
 
 const registerUser = asyncHandler(async (req, res) => {
     const {username,email, password} = req.body
-    // NOTE:request body console log karani h
     console.log("email", email);
     if (
         [email,username,password].some((field)=>
@@ -17,8 +32,6 @@ const registerUser = asyncHandler(async (req, res) => {
     if (existedUser) {
         throw new ApiError(409, "User already exists with this email/username");
     }
-    // NOTE: request files console log karani h
-    // console.log("req.files", req.files);
 
     const user = await User.create({
         email,
@@ -39,4 +52,75 @@ const registerUser = asyncHandler(async (req, res) => {
 
 });
 
-export { registerUser };
+const loginUser = asyncHandler(async (req, res) => {
+
+    const {username , email , password} = req.body
+    // if (!(username || email)) {
+    if (!(username || email)) {
+        throw new ApiError(400, "Username or email is required");
+    }
+
+    const user = await User.findOne({
+        $or: [{email}, {username}]
+    })
+
+    if (!user) {
+        throw new ApiError(404, "User not found");
+    }
+
+    const isPasswordValid = await user.comparePassword(password)
+
+    if (!isPasswordValid) {
+        throw new ApiError(401, "Password incorrect");
+    }
+
+    const {accessToken, refreshToken} = await generateAccessAndRefreshToken(user._id);
+
+    const loggedInUser = await User.findById(user._id).select("-password -refreshToken");
+
+    const options = {
+        httpOnly: true,
+        secure: true
+        // sameSite: "strict",
+        // maxAge: 7 * 24 * 60 * 60 * 1000 // 7 days
+    }
+
+    return res
+    .status(200)
+    .cookie("accessToken", accessToken, options)
+    .cookie("refreshToken", refreshToken, options)
+    .json(
+        new ApiResponse(200, {
+            user: loggedInUser,
+            accessToken,
+            refreshToken
+        }, "User logged in successfully")
+    )
+})
+
+const logoutUser = asyncHandler(async (req, res) => {
+    await User.findByIdAndUpdate(
+        req.user._id,
+        {
+            $set: {
+                refreshToken: undefined 
+            }
+        },
+        {
+            new: true,
+        }
+    )
+    const options = {
+        httpOnly: true,
+        secure: true,
+    }
+    return res
+    .status(200)
+    .clearCookie("accessToken", options)
+    .clearCookie("refreshToken", options)
+    .json(
+        new ApiResponse(200, {}, "User logged out successfully")
+    ) 
+});
+
+export { registerUser, loginUser, logoutUser };
--- src/models/user.models.js
+++ src/models/user.models.js
@@ -40,13 +40,17 @@ const userSchema = new Schema({
         },
 }, {timestamps: true});
 
-userSchema.pre("save", async function (next) {
-    if (!this.isModified("password")) {
-        return next();
-    }
+// userSchema.pre("save", async function(next){
+//     if (!this.isModified("password")) return next();
+//     this.password = await bcrypt.hash(this.password, 10);
+//     next();
+// })
+
+userSchema.pre("save", async function () {
+    if (!this.isModified("password")) return;
     this.password = await bcrypt.hash(this.password, 10);
-    next();
-})
+});
+
 
 userSchema.methods.comparePassword = async function (password) {
     return await bcrypt.compare(password, this.password);
