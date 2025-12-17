f361298f3527274b8b652978073a054c60d5a510
Sabir <sabiralitanveer6@gmail.com>
2025-12-17 18:11:22 +0530

    add deletePost, getUserPosts, and getFeed functionalities with corresponding routes

--- src/controllers/post.controllers.js
+++ src/controllers/post.controllers.js
@@ -1,5 +1,6 @@
 import { asyncHandler } from "../utils/asyncHandler.js";
 import { Post } from "../models/Post.models.js";
+import { Follow } from "../models/Follow.models.js";
 import { uploadOnCloudinary } from "../utils/cloudinary.js";
 import { ApiError } from "../utils/ApiError.js";
 import { ApiResponse } from "../utils/ApiResponse.js";
@@ -34,4 +35,41 @@ const getPostById = asyncHandler(async (req, res) => {
     )
 })
 
-export {createPost, getPostById};
+const deletePost = asyncHandler(async (req, res) => {
+    const post = await Post.findById({_id: req.params.id, author: req.user._id});
+    if (!post) {
+        throw new ApiError(404, "Post not found or you are not authorized to delete this post");
+    }
+    await post.deleteOne()
+    return res.status(200).json(
+        new ApiResponse(200, null, "Post deleted successfully")
+    )
+})
+
+const getUserPosts = asyncHandler(async (req, res) => {
+    const posts = await Post.find({author: req.params.userId}).sort({createdAt: -1});
+    return res.status(200).json(
+        new ApiResponse(200, posts, "User posts fetched successfully")
+    )
+})
+
+const getFeed = asyncHandler(async (req, res) => {
+    const page = Number(req.query.page) || 1;
+    const limit = Number(req.query.limit) || 10;
+    const skip = (page - 1) * limit;
+
+    const following = await Follow.find({ follower: req.user._id }).select("following");
+    const followingIds = following.map((f) => f.following);
+
+    const posts = await Post.find({
+      author: { $in: [...followingIds, req.user._id] },
+    })
+      .sort({ createdAt: -1 })
+      .skip(skip)
+      .limit(limit)
+      .populate("author", "username avatarUrl");
+
+    res.json(posts);
+});
+
+export {createPost, getPostById, deletePost, getUserPosts, getFeed};
