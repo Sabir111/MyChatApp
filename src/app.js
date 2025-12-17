ebd9398e1eb62d647711a9219ff02f86fba087e1
Sabir <sabiralitanveer6@gmail.com>
2025-12-17 19:10:20 +0530

    add routes for likes, comments, follows, and notifications

--- src/app.js
+++ src/app.js
@@ -19,11 +19,19 @@ app.use(cookieParser());
 //import routes here
 import userRouter from './routes/user.routes.js';
 import postRouter from './routes/post.routes.js';
+import likeRouter from './routes/like.routes.js';
+import commentRouter from './routes/comment.routes.js';
+import followRouter from './routes/follow.routes.js';
+import notificationRouter from './routes/notification.routes.js';
 
 //routes declaration here
 
 app.use('/api/v1/users', userRouter);
 app.use('/api/v1/posts', postRouter);
+app.use('/api/v1/likes', likeRouter);
+app.use('/api/v1/comments', commentRouter);
+app.use('/api/v1/follows', followRouter);
+app.use('/api/v1/notifications', notificationRouter);
 
 
 export default app;
